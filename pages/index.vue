1651d5f5904903e816e1c0bdbb7a8bc25ad4ab59
yboulemi <yanis.boulemia@gmail.com>
2024-03-28 16:46:37 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>99 Numbers</h1>
+    <NuxtLink to="/page2">Go to page2</NuxtLink>
+    <ul></ul>
+  </div>
+</template>
---
+++ pages/page2.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>page2</h1>
+    <NuxtLink to="/">Go to page2</NuxtLink>
+    <ul></ul>
+  </div>
+</template>
