ad0a57f90c1f8b075c3d6536ae81965dd2e498a3
Grégoy de Pass <g2pass@gmail.com>
2024-04-04 23:04:48 +0800

    feature: install i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss"],
+    modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
     devtools: { enabled: true },
 });
